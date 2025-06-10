ccc976d2e85adbb6436768a48f10c3cc296133b6
Andrei Radovici <andrei.radovici@fuuse.io>
2025-06-10 14:41:04 +0100

    Icons and layout updates

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,7 +5,16 @@ export const metadata = {
   title: "FindPalco",
   description: "Connect Beyond Travel",
   icons: {
-    icon: "/favicon-palco.png",
+    icon: [
+      { url: "/favicon.ico", type: "image/x-icon" },
+      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+    ],
+    apple: [{ url: "/apple-touch-icon.png", sizes: "180x180" }],
+  },
+  manifest: "/site.webmanifest",
+  other: {
+    "theme-color": "#ffffff",
   },
 };
 
