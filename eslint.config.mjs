835977f656bc8965a60f4fb329de59953954128d
Andrei Radovici <andrei.radovici@fuuse.io>
2025-06-11 15:56:44 +0100

    Storybook setup

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,6 @@
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+import storybook from "eslint-plugin-storybook";
+
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
 import { FlatCompat } from '@eslint/eslintrc';
@@ -9,11 +12,8 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [
-  {
-    ignores: ['.next/', 'node_modules/'],
-  },
-  ...compat.extends('next/core-web-vitals'),
-];
+const eslintConfig = [{
+  ignores: ['.next/', 'node_modules/'],
+}, ...compat.extends('next/core-web-vitals'), ...storybook.configs["flat/recommended"]];
 
 export default eslintConfig;
