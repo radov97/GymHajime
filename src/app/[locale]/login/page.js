1c025bd4e69eb1bdc527ea8479850293610b76ee
Andrei Radovici <andrei.radovici@fuuse.io>
2025-06-15 21:30:15 +0100

    Login page form validations

--- src/app/[locale]/login/page.js
+++ src/app/[locale]/login/page.js
@@ -8,20 +8,62 @@ import ButtonPalco from '@/components/ButtonPalco';
 import GoogleButtonPalco from '@/components/GoogleButtonPalco';
 import Link from 'next/link';
 import { ButtonRank, ButtonType, InputType } from '@/lib/enums';
-import { isNotEmptyString } from '@/lib/helperFunctions';
+import { isNotEmptyString, isValidEmail, useValidatedPassword } from '@/lib/helperFunctions';
 import { useLocale, useTranslations } from 'next-intl';
 import FormContainerPalco from '@/components/FormContainerPalco';
 import ClientOnly from '@/lib/ClientOnly';
 
 export default function LoginPage() {
   const [email, setEmail] = useState('');
+  const [hasEmailError, setHasEmailError] = useState(false);
+  const [isEmailTouched, setIsEmailTouched] = useState(false);
+  const [emailErrorText, setEmailErrorText] = useState('');
+
   const [password, setPassword] = useState('');
+  const [hasPasswordError, setHasPasswordError] = useState(false);
+  const [isPasswordTouched, setIsPasswordTouched] = useState(false);
+  const [passwordErrorText, setPasswordErrorText] = useState('');
+
+  const [isTyping, setIsTyping] = useState(false);
   const [loading, setLoading] = useState(false);
+
   const router = useRouter();
   const searchParams = useSearchParams();
   const errorParam = searchParams.get('error');
   const locale = useLocale();
-  const t = useTranslations('login');
+  const t = useTranslations();
+  const validatePassword = useValidatedPassword();
+
+  const handleEmailDebounced = (val) => {
+    if (isValidEmail(val)) {
+      setHasEmailError(false);
+      setEmailErrorText('');
+    } else {
+      setHasEmailError(true);
+      setEmailErrorText(t('validations.has-email-error'));
+    }
+  };
+
+  const handlePasswordDebounced = (val) => {
+    const errors = validatePassword(val);
+
+    if (errors.length === 0) {
+      setHasPasswordError(false);
+      setPasswordErrorText('');
+    } else {
+      setHasPasswordError(true);
+      setPasswordErrorText(errors[0]); // Show the first error for clarity
+    }
+  };
+
+  const isFormValid =
+    isEmailTouched &&
+    isPasswordTouched &&
+    !hasEmailError &&
+    !hasPasswordError &&
+    email.trim() !== '' &&
+    password.trim() !== '' &&
+    !isTyping;
 
   const handleLogin = async (e) => {
     e.preventDefault();
@@ -40,36 +82,49 @@ export default function LoginPage() {
   };
 
   async function signInWithGoogle() {
-    // await supabase.auth.signInWithOAuth({ provider: 'google' });
-    // check here what to do
+    await supabase.auth.signInWithOAuth({ provider: 'google' });
   }
 
   return (
     <FormContainerPalco>
       <h2 className="text-2xl font-bold text-[var(--color-palco-black)] mb-2 text-center cursor-default">
-        {t('login')}
+        {t('login.login')}
       </h2>
       <p className="mb-4 text-sm text-gray-600 text-center cursor-default">
-        {t('welcome-message')}
+        {t('login.welcome-message')}
       </p>
-      <GoogleButtonPalco text={t('google-sign-in')} onClick={signInWithGoogle} />
+      <GoogleButtonPalco text={t('login.google-sign-in')} onClick={signInWithGoogle} />
 
       <ClientOnly>
         <form onSubmit={handleLogin} className="space-y-4">
           <InputPalco
             type={InputType.Email}
-            placeholder={t('email')}
+            placeholder={t('login.email')}
             value={email}
-            onChange={(e) => setEmail(e.target.value)}
+            onChange={(e) => {
+              if (!isEmailTouched) setIsEmailTouched(true);
+              setEmail(e.target.value);
+            }}
+            onDebouncedChange={handleEmailDebounced}
+            error={hasEmailError}
+            errorText={emailErrorText}
             required
+            setIsTyping={setIsTyping}
           />
           <InputPalco
             type={InputType.Password}
-            placeholder={t('password')}
+            placeholder={t('login.password')}
             value={password}
-            onChange={(e) => setPassword(e.target.value)}
+            onChange={(e) => {
+              if (!isPasswordTouched) setIsPasswordTouched(true);
+              setPassword(e.target.value);
+            }}
+            onDebouncedChange={handlePasswordDebounced}
+            error={hasPasswordError}
+            errorText={passwordErrorText}
             required
             showToggle
+            setIsTyping={setIsTyping}
           />
 
           <button
@@ -79,27 +134,28 @@ export default function LoginPage() {
             }}
             className="text-[var(--color-palco)] text-sm font-medium hover:underline text-left cursor-pointer"
           >
-            {t('forgot-password')}
+            {t('login.forgot-password')}
           </button>
 
           <ButtonPalco
             type={ButtonType.Submit}
             rank={ButtonRank.Primary}
-            text={t('login')}
+            text={t('login.login')}
             loading={loading}
-            className="mt-2"
+            disabled={!isFormValid}
           />
         </form>
       </ClientOnly>
+
       {errorParam && <p className="text-red-600 mt-4">{decodeURIComponent(errorParam)}</p>}
 
       <p className="mt-4 text-sm text-center cursor-default">
-        {t('new-user')}{' '}
+        {t('login.new-user')}{' '}
         <Link
           href={`/${locale}/signup`}
           className="text-[var(--color-palco)] text-sm font-medium hover:underline"
         >
-          {t('sign-up')}
+          {t('login.sign-up')}
         </Link>
       </p>
     </FormContainerPalco>
